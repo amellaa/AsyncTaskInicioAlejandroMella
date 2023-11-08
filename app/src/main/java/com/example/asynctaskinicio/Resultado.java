069dbcb73fc36be79e28e6232b2e568ace46017d
alejandro <a.mellab@alumnos.santotomas.cl>
2023-11-08 19:13:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/asynctaskinicio/Resultado.java
@@ -0,0 +1,21 @@
+package com.example.asynctaskinicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Resultado extends AppCompatActivity {
+    private TextView Resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        Resultado = findViewById(R.id.et_resultado);
+        String nombreUsuario =getIntent().getStringExtra("Usuario");
+        Resultado.setText("Bienvenido"+ nombreUsuario);
+    }
+}
